266af80d25864998f03fc654202405aa8147d3ed
Teeranat-569 <crystals.nbk88@gmail.com>
2020-12-03 17:44:59 +0700

    edited**

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
-    <KanBan :data="data" :create_task_submit="create_task_submit"/>
+    <KanBan
+      :data="data"
+      :create_task_submit="create_task_submit"
+      :move_item_task="move_item_task"
+    />
   </div>
 </template>
 
@@ -12,40 +16,44 @@ export default {
   components: {
     KanBan,
   },
-  data(){
+  data() {
     return {
-      data:[
+      data: [
         {
-          name :"todo",
-          color:"#F4B58E",
-          tasks:[
-            {task_name: "test"}
-          ]
+          name: "todo",
+          color: "#F4B58E",
+          tasks: [{ task_name: "test" }],
         },
         {
-          name :"doing",
-          color:"#F363B1",
-          tasks:[ {task_name: "test"}]
+          name: "doing",
+          color: "#F363B1",
+          tasks: [{ task_name: "test" }],
         },
         {
-          name :"done",
-          color:"#FF9420",
-          tasks:[ {task_name: "test"}]
-        }
-      ]
-    }
+          name: "done",
+          color: "#FF9420",
+          tasks: [{ task_name: "test" }],
+        },
+      ],
+    };
   },
-  methods:{
-    create_task_submit(column_index,item){
-      this.data[column_index].tasks.push(item)
-    }
-  }
+  methods: {
+    create_task_submit(column_index, item) {
+      this.data[column_index].tasks.push(item);
+    },
+    move_item_task(column_a, index_task_a, column_b, index_task_b) {
+      var item = this.data[column_a].tasks[index_task_a];
+      //remove task before
+      this.data[column_a].tasks.splice(index_task_a, 1);
+      //move to
+      this.data[column_b].tasks.splice(index_task_b, 0, item);
+    },
+  },
 };
 </script>
 
 <style>
 #app {
-
   height: 100vh;
 }
 </style>
